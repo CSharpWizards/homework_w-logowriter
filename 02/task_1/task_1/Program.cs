3d9b45503046e5fb5a90ddf2c78ebe74e29e5105
Nick <lubovskiy.nick@gmail.com>
2018-04-04 23:46:26 +0300

    Homework_2

---
+++ 02/task_1/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter three words:");
+
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+            string s3 = Console.ReadLine();
+
+            Console.WriteLine(s3);
+            Console.WriteLine(s2);
+            Console.WriteLine(s1);
+        }
+    }
+}
---
+++ 02/task_2/task_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = "world123";
+            string b = "hello";
+
+            string copy_a = a;
+            a = b;
+            b = copy_a;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+    }
+}
