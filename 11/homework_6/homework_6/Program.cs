09b0a1393af283937dc3d545e00cf6f1faa7fd6c
Николай <lubovskiy.nick@gmail.com>
2018-05-10 17:34:44 +0300

    Домашнаяя работа 6

---
+++ 11/homework_6/homework_6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите действительное число");
+            double easy_task = double.Parse(Console.ReadLine());
+            if (easy_task < 0)
+            {
+                Console.WriteLine(easy_task * -1);
+            }
+            else
+            {
+                Console.WriteLine(easy_task);
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ 11/homework_6/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите количестово очков которые набрала футбольная команда");
+            int ochki1 = int.Parse(Console.ReadLine());
+
+            if (ochki1 == 1 )
+            {
+                Console.WriteLine("ничья");
+            }
+            else if (ochki1 == 0)
+            {
+                Console.WriteLine("поражение");
+            }
+            else if (ochki1 == 3)
+            {
+                Console.WriteLine("выигрыш ");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
