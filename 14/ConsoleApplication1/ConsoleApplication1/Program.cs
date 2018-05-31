cc6f7cf3cc6daf86482675b3e6633c0730101676
logowriter <lubovskiy.nick@gmail.com>
2018-05-31 20:56:30 +0300

    14

---
+++ 14/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("это программа управления летающей тарелки, которая ворует коровок (не конфеты). Собственно перейдем к делу наша расса хочет спереть у глупых землян   парочку сотен коров. Каждая корова весит 1-ну единицу измерения,а летающая тарелка может выдержать только 500 единиц. Введи сколько коров ты хочешь спереть потом введи грузоподъемность корабля");
+            int maxmasscow = 0; 
+            int massCow = int.Parse(Console.ReadLine());
+            int massShip = int.Parse(Console.ReadLine()); 
+            while(true){
+                if (massCow <= massShip)
+                {
+                    Console.WriteLine("ужин будет вкусный");
+
+                }
+                else
+                {
+                    Console.WriteLine("эхх в следующий раз возьмем тарелку побольше");
+                }
+               
+            }
+            Console.ReadLine();
+        }
+    }
+}
--- 14/MemoryTrainer/Program.cs
+++ 14/MemoryTrainer/Program.cs
@@ -14,14 +14,14 @@ namespace MemoryTrainer {
 		static void GuessNumber() {
 			// создаем генератор случайных чисел
 			Random rand = new Random();
-
+            int rightNumbers = 0;
 			int maxTarget = 100;
+            int time = 2000;
 			while (true) {
 				// генерируем случайное число с помощью генератора
 				int target = rand.Next(maxTarget);
-
-				Console.WriteLine("Запомните число: " + target);
-				Thread.Sleep(2000);
+                Console.WriteLine("Запомните число: " + target);
+				Thread.Sleep(time);
 				Console.Clear();
 
 				Console.WriteLine("Введите запомненное число");
@@ -30,12 +30,18 @@ namespace MemoryTrainer {
 
 				if (guess == target) {
 					Console.WriteLine("Вы угадали!");
+                    rightNumbers=rightNumbers + 1;
 					maxTarget = maxTarget + 200;
+                    if (rightNumbers >= 1)
+                    {
+                        time = time-1000;
+                    }
 				}
 				else {
 					Console.WriteLine("Вы ошиблись ='(");
 					maxTarget = maxTarget - 100;
 				}
+               
 				Thread.Sleep(1000);
 				Console.Clear();
 			}
